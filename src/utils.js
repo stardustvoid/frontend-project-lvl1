9abca696d0901ec768cfc7589bcad9c527904ca3
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2023-03-27 19:56:07 +0700

    Add utils

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const isEven = (num) => num % 2 === 0;
+
+const boolToYesNo = (bool) => (bool ? 'yes' : 'no');
+
+const getMathOperator = () => {
+  const mathOperators = ['+', '-', '*'];
+  const operatorsCount = mathOperators.length;
+  const operatorIndex = generateRandomNum(0, operatorsCount - 1);
+
+  return mathOperators[operatorIndex];
+};
+
+const evaluateMathExpression = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return false;
+  }
+};
+
+export { generateRandomNum, isEven, boolToYesNo, getMathOperator, evaluateMathExpression };
